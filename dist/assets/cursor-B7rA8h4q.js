f3a770cc692cd038795b4e31efa2c3a89347f0f9
itskumar666 <ashutosh666be@Gmail.com>
2025-09-10 09:48:52 +0530

    Fix Vercel deployment configuration
    - Updated vercel.json to use modern configuration format
    - Simplified build process to use Vite directly
    - Removed problematic TypeScript check from build script
    - Fixed module resolution issues for deployment

--- dist/assets/cursor-B7rA8h4q.js
+++
@@ -1,3 +0,0 @@
-import{b as o}from"./core-CjE3ST9r.js";import"./index-DXkzWzuW.js";import"./events-8636V3zY.js";import"./index.es-BbDcu3-V.js";import"./index-nibyPLVP.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{l as cursorSvg};
---
+++ dist/assets/cursor-CR4eep6b.js
@@ -0,0 +1,3 @@
+import{b as o}from"./core-C5F37Nwz.js";import"./index-C2j2xkph.js";import"./events-DKGz4Vw9.js";import"./index.es-Dn4ySBRb.js";import"./index-nibyPLVP.js";const l=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{l as cursorSvg};
